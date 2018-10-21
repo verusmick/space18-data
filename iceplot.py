d2d6d5de9afcd391547ef76e5014da59b1c99570
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2018-10-21 08:27:31 +0200

    plot flat world

--- iceplot.py
+++ iceplot.py
@@ -10,8 +10,17 @@ os.environ["PROJ_LIB"] = proj_lib
 
 from mpl_toolkits.basemap import Basemap
 
-plt.figure(figsize=(8, 8))
-m = Basemap(projection='ortho', resolution=None, lat_0=50, lon_0=-100)
-m.bluemarble(scale=0.5);
+# plt.figure(figsize=(8, 8))
+# m = Basemap(projection='ortho', resolution=None, lat_0=50, lon_0=-100)
+# m.bluemarble(scale=0.5);
+# plt.show()
 
-plt.show()
+def plot_world_flat(longitude, latitude, data):
+    plt.figure(figsize=(8, 8))
+    m = Basemap(projection='cyl', resolution='l', llcrnrlat=-90, urcrnrlat=90, llcrnrlon=-180, urcrnrlon=180)
+    m.drawcoastlines(linewidth=0.5)
+    m.drawparallels(np.arange(-90., 120., 30.), labels=[1, 0, 0, 0])
+    m.drawmeridians(np.arange(-180., 181., 45.), labels=[0, 0, 0, 1])
+    x, y = m(longitude, latitude)
+    m.pcolormesh(x, y, data)
+    plt.show()
